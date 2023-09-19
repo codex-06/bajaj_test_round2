6f21482376daae76720ae6c34794a4725fe6f471
Aryan <aryan602.as@gmail.com>
2023-09-19 17:09:13 +0530

    Final Code

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+
+
+
+app.get('/api', (req, res) => {
+  res.json({ operation_code: 1 });
+});
+
+
+app.post('/api', (req, res) => {
+  try {
+    const data = req.body.data;
+
+    // const numbers = data.numbers;
+    // const alphabets = data.alphabets;
+
+    // // Find the highest alphabet
+    // const highestAlphabet = alphabets.reduce((max, current) => max > current ? max : current);
+    const numbers = [];
+    const alphabets = [];
+
+    // Loop through the input list and separate numbers and alphabets
+    for (const item of data) {
+        if (typeof item === 'number' || !isNaN(parseInt(item))) {
+        // Check if the item is a number
+        numbers.push(item);
+        } else if (typeof item === 'string' && item.length === 1 && item.match(/[a-zA-Z]/)) {
+        // Check if the item is a single alphabet character
+        alphabets.push(item);
+        }
+    }
+
+    // Find the maximum alphabet among the alphabets
+    const maxAlphabet = alphabets.reduce((max, current) => (current > max ? current : max), '');
+
+    const response = {
+      is_success: true,
+      user_id: "aryan_sharma_06022001",
+      email: "aryan.sharma2020@vitbhopal.ac.in",
+      roll_number: "20BCE109837",
+      "numbers": numbers,
+      "alphabets": alphabets,
+      "highest_alphabet": maxAlphabet!=""?[maxAlphabet]:[]
+    };
+
+    res.json(response);
+  } catch (error) {
+    res.status(400).json({ status: "Error", message: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
